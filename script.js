9e483fcc4f352f2964d52d7f6509ce10a6790e28
Ayush Thakur <ayt12321232@gmail.com>
2024-03-04 00:20:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let num;
+let count = 0;
+let result;
+let correctAudio = new Audio('sounds/correct-6033.mp3');
+let wrongAudio = new Audio('sounds/wronganswer-37702.mp3');
+let failedAudio = new Audio('sounds/fail-jingle-stereo-mix-88784.mp3');
+
+function generateNum() {
+    num = Math.ceil(Math.random() * 10);
+}
+generateNum();
+
+Array.from(document.getElementsByClassName("btn")).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (num == button.value) {
+            button.style.color = "white";
+            button.style.backgroundColor = "rgb(114, 246, 114)";
+            count++;
+            button.disabled = true;
+            result = 11 - count;
+            correctAudio.play();
+            document.getElementById("passed").innerHTML = "Great, You done it. Your score was " + result;
+            Array.from(document.getElementsByClassName("btn")).forEach((button) => {
+                button.disabled = true;
+            })
+        }
+        else {
+            button.style.color = "white";
+            button.style.backgroundColor = "rgb(255, 119, 119)";
+            count++;
+            button.disabled = true;
+            wrongAudio.play();
+            Array.from(document.getElementsByClassName("btn")).forEach((button) => {
+                if (count == 3) {
+                    button.disabled = true;
+                    failedAudio.play();
+                    document.getElementById("failed").innerHTML = "You Failed ! The number was " + num;
+                }
+            })
+        }
+    })
+})
+
