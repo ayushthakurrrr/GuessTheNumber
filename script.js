c4b01872ba01bc9559df2f65180e2e4a182c88ff
Ayush Thakur <ayt12321232@gmail.com>
2024-03-10 18:03:37 +0530

    New commit

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 let num;
 let count = 0;
 let result;
+
 let correctAudio = new Audio('sounds/correct-6033.mp3');
 let wrongAudio = new Audio('sounds/wronganswer-37702.mp3');
-let failedAudio = new Audio('sounds/fail-jingle-stereo-mix-88784.mp3');
+
+const Btn = document.getElementsByClassName("btn");
+const chancesLeft = document.getElementById("left");
+let passedMsg = document.getElementById("passed");
+let failedMsg = document.getElementById("failed");
 
 function generateNum() {
     num = Math.ceil(Math.random() * 10);
 }
 generateNum();
-Array.from(document.getElementsByClassName("btn")).forEach((button) => {
+Array.from(Btn).forEach((button) => {
     button.addEventListener('click', (e) => {
         if (num == button.value) {
             button.style.color = "white";
@@ -19,13 +24,13 @@ Array.from(document.getElementsByClassName("btn")).forEach((button) => {
             result = 11 - count;
             correctAudio.play();
             if (count == 1) {
-                document.getElementById("left").innerText = 3 - count + ' chances were left';
+                chancesLeft.innerText = (3 - count) + ' chances were left';
             }
             else {
-                document.getElementById("left").innerText = 3 - count + ' chance was left';
+                chancesLeft.innerText = (3 - count) + ' chance was left';
             }
-            document.getElementById("passed").innerHTML = "Great, You done it. Your score was " + result;
-            Array.from(document.getElementsByClassName("btn")).forEach((button) => {
+            passedMsg.innerHTML = "Great, You done it. Your score was " + result;
+            Array.from(Btn).forEach((button) => {
                 button.disabled = true;
             })
         }
@@ -35,17 +40,16 @@ Array.from(document.getElementsByClassName("btn")).forEach((button) => {
             count++;
             button.disabled = true;
             if (count == 1) {
-                document.getElementById("left").innerText = 3 - count + ' chances left';
+                chancesLeft.innerText = (3 - count) + ' chances left';
             }
             else {
-                document.getElementById("left").innerText = 3 - count + ' chance left';
+                chancesLeft.innerText = (3 - count) + ' chance left';
             }
             wrongAudio.play();
-            Array.from(document.getElementsByClassName("btn")).forEach((button) => {
+            Array.from(Btn).forEach((button) => {
                 if (count == 3) {
                     button.disabled = true;
-                    failedAudio.play();
-                    document.getElementById("failed").innerHTML = "You Failed ! The number was " + num;
+                    failedMsg.innerHTML = "You Failed! The number was " + num;
                 }
             })
         }
